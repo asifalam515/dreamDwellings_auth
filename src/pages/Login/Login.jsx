5db4fd148466d346c01c44c2a3cba82576330d50
asifalam515 <asibulalam515@gmail.com>
2024-09-21 16:34:36 +0600

    github login system applied

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -10,6 +10,7 @@ import {
   signInWithPopup,
 } from "firebase/auth";
 import { app } from "../../firebase/firebase.config";
+import { GithubAuthProvider } from "firebase/auth";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -20,6 +21,7 @@ const Login = () => {
 
   const auth = getAuth(app);
   const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
   const handleGoogleLogin = () => {
     signInWithPopup(auth, googleProvider)
@@ -37,6 +39,23 @@ const Login = () => {
       })
       .catch((error) => {
         console.log("Error while using Google login", error);
+      });
+  };
+  // github sign in
+  const handleGithubSignin = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        const newUser = result.user;
+        const name = newUser.displayName;
+        const photo = newUser.photoURL;
+
+        setName(name);
+        setPhoto(photo);
+        localStorage.setItem("name", name);
+        localStorage.setItem("photo", photo);
+      })
+      .catch((error) => {
+        console.log("github error", error.message);
       });
   };
 
@@ -137,7 +156,11 @@ const Login = () => {
               <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
             </svg>
           </button>
-          <button aria-label="Log in with GitHub" className="p-3 rounded-sm">
+          <button
+            onClick={handleGithubSignin}
+            aria-label="Log in with GitHub"
+            className="p-3 rounded-sm"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 32 32"
